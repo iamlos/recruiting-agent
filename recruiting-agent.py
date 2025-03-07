8c5c2f153fbdb932841d34e353dc05a31f84495e
iamlos <iamlos@users.noreply.github.com>
2025-03-07 17:26:14 -0500

    Update recruiting-agent.py
    added pysqlite3

--- recruiting-agent.py
+++ recruiting-agent.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from crew import RecruitingCrew
 from gsheets_utils import GSheetManager
